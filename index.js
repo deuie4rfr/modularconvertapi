b7ed9d599624fd6e54b3333b0e65a743431a097f
deuie4rfr <rsjcawrwarawraw@hotmail.com>
2024-05-07 13:05:48 +0700

    demo project

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function factorial(n) {
-  if (n === 0 || n === 1) return 1;
-  return n * factorial(n - 1);
+function decodeString(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char !== "]") stack.push(char);
+    else {
+      let str = "";
+      while (stack.length && stack[stack.length - 1] !== "[")
+        str = stack.pop() + str;
+      stack.pop();
+      let num = "";
+      while (stack.length && !isNaN(stack[stack.length - 1]))
+        num = stack.pop() + num;
+      stack.push(str.repeat(parseInt(num)));
+    }
+  }
+  return stack.join("");
 }
