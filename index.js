06b97f501b2f715515bb91857a17848edf81705e
deuie4rfr <rsjcawrwarawraw@hotmail.com>
2024-05-08 13:29:32 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function decodeString(s) {
-  const stack = [];
-  for (const char of s) {
-    if (char !== "]") stack.push(char);
-    else {
-      let str = "";
-      while (stack.length && stack[stack.length - 1] !== "[")
-        str = stack.pop() + str;
-      stack.pop();
-      let num = "";
-      while (stack.length && !isNaN(stack[stack.length - 1]))
-        num = stack.pop() + num;
-      stack.push(str.repeat(parseInt(num)));
-    }
-  }
-  return stack.join("");
-}
+const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
