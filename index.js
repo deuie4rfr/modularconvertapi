a8a15d887d46cbab563cca64350b56b758bc299c
deuie4rfr <rsjcawrwarawraw@hotmail.com>
2024-05-11 13:33:57 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-function buildTree(inorder, postorder) {
-  if (!inorder.length || !postorder.length) return null;
-  const rootVal = postorder[postorder.length - 1];
-  const root = new TreeNode(rootVal);
-  const mid = inorder.indexOf(rootVal);
-  root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid));
-  root.right = buildTree(
-    inorder.slice(mid + 1),
-    postorder.slice(mid, postorder.length - 1),
-  );
-  return root;
+function combinationSum2(candidates, target) {
+  candidates.sort((a, b) => a - b);
+  const result = [];
+  backtrack([], 0, 0);
+  return result;
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
+      result.push([...combination]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      if (i > start && candidates[i] === candidates[i - 1]) continue;
+      combination.push(candidates[i]);
+      backtrack(combination, i + 1, sum + candidates[i]);
+      combination.pop();
+    }
+  }
 }
